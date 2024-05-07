c54a4642fd1419b0c5382c350f0c8ee1e3ef15d3
aakash2330 <aakash2330@gmail.com>
2024-05-08 02:34:04 +0530

    bonk

--- src/components/form/symbolPopover.tsx
+++ src/components/form/symbolPopover.tsx
@@ -1,9 +1,8 @@
+"use client";
 
-"use client"
- 
-import * as React from "react"
- 
-import { Button } from "@/components/ui/button"
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
 import {
   Command,
   CommandEmpty,
@@ -11,126 +10,124 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-} from "@/components/ui/command"
-import {
-  Drawer,
-  DrawerContent,
-  DrawerTrigger,
-} from "@/components/ui/drawer"
+} from "@/components/ui/command";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
-import { useMediaQuery } from "@/hooks/use-media-query"
-import { useRouter } from "next/navigation"
- 
-type Status = {
-  value: string
-  label: string
-}
- 
- 
-export function ComboBoxResponsive({ symbolData }: { symbolData: string[] }) {
-  const [open, setOpen] = React.useState(false)
-  const isDesktop = useMediaQuery("(min-width: 768px)")
+} from "@/components/ui/popover";
+import { useMediaQuery } from "@/hooks/use-media-query";
+import { useRouter } from "next/navigation";
 
-  const [selectedStatus, setSelectedStatus] = React.useState<Status | null>(
-    null
-  )
- 
+export function ComboBoxResponsive({ symbolData }: { symbolData: string[] }) {
+  const [open, setOpen] = React.useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  const [selectedStatus, setSelectedStatus] = React.useState<string | null>(
+    null,
+  );
+
   if (isDesktop) {
     return (
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
           <Button variant="outline" className="w-[150px] justify-start">
-            {selectedStatus ? <>{selectedStatus.label}</> : <>+ Set status</>}
+            {selectedStatus ? <>{selectedStatus}</> : <>+ Set status</>}
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-[200px] p-0" align="start">
-          <StatusList setOpen={setOpen} symbolData={symbolData} setSelectedStatus={setSelectedStatus} />
+          <StatusList
+            setOpen={setOpen}
+            symbolData={symbolData}
+            setSelectedStatus={setSelectedStatus}
+          />
         </PopoverContent>
       </Popover>
-    )
+    );
   }
- 
+
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>
         <Button variant="outline" className="w-[150px] justify-start">
-          {selectedStatus ? <>{selectedStatus.label}</> : <>+ Set status</>}
+          {selectedStatus ? <>{selectedStatus}</> : <>+ Set status</>}
         </Button>
       </DrawerTrigger>
       <DrawerContent>
         <div className="mt-4 border-t">
-          <StatusList setOpen={setOpen} symbolData={symbolData} setSelectedStatus={setSelectedStatus} />
+          <StatusList
+            setOpen={setOpen}
+            symbolData={symbolData}
+            setSelectedStatus={setSelectedStatus}
+          />
         </div>
       </DrawerContent>
     </Drawer>
-  )
+  );
 }
- 
+
 function StatusList({
   setOpen,
-  symbolData
+  setSelectedStatus,
+  symbolData,
 }: {
-  setOpen: (open: boolean) => void
-  setSelectedStatus: (status: Status | null) => void
-    symbolData:string[]
+  setOpen: (open: boolean) => void;
+  setSelectedStatus: (status: string | null) => void;
+  symbolData: string[];
 }) {
-
   const router = useRouter();
   const [symbols, setSymbols] = React.useState<string[]>(symbolData);
-  const [selectedSymbol, setSelectedSymbol] = React.useState<string | null>(null);
+  const [selectedSymbol, setSelectedSymbol] = React.useState<string | null>(
+    null,
+  );
   return (
     <Command>
-            <CommandInput
-              onChangeCapture={(e:any) => {
-                setSymbols((prev) => {
-                  const newNames = symbolData.filter((s) => {
-                    if (
-                      s.startsWith(
-                        (e.target as HTMLInputElement).value.toUpperCase(),
-                      ) ||
-                      s
-                        .slice(1)
-                        .startsWith(
-                          (e.target as HTMLInputElement).value.toUpperCase(),
-                        )
-                    ) {
-                      return true;
-                    }
-                  });
-                  return newNames;
-                });
-              }}
-              placeholder="Select Symbol..."
-            />
+      <CommandInput
+        onChangeCapture={(e: any) => {
+          setSymbols((prev) => {
+            const newNames = symbolData.filter((s) => {
+              if (
+                s.startsWith(
+                  (e.target as HTMLInputElement).value.toUpperCase(),
+                ) ||
+                s
+                  .slice(1)
+                  .startsWith(
+                    (e.target as HTMLInputElement).value.toUpperCase(),
+                  )
+              ) {
+                return true;
+              }
+            });
+            return newNames;
+          });
+        }}
+        placeholder="Select Symbol..."
+      />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
         <CommandGroup>
           {symbols.map((symbol, index) => {
-                  return (
-                    index < 10 && (
-                      <CommandItem
-                        key={index}
-                        value={symbol}
-                        onSelect={(value:string) => {
-                          setSelectedSymbol(
-                            symbols.find((priority) => priority === value) ||
-                              null,
-                          );
-                          setOpen(false);
-                          router.push(`/graph?symbol=${value}`);
-                        }}
-                      >
-                        <span>{symbol}</span>
-                      </CommandItem>
-                    )
-                  );
-                })}
+            return (
+              index < 10 && (
+                <CommandItem
+                  key={index}
+                  value={symbol}
+                  onSelect={(value: string) => {
+                    setSelectedStatus(
+                      symbols.find((priority) => priority === value) || null,
+                    );
+                    setOpen(false);
+                    router.push(`/graph?symbol=${value}`);
+                  }}
+                >
+                  <span>{symbol}</span>
+                </CommandItem>
+              )
+            );
+          })}
         </CommandGroup>
       </CommandList>
     </Command>
-  )
+  );
 }
